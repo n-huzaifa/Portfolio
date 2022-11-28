0968d1b19823436af929c4949d9ea18e4008147e
n-huzaifa <noorhuzaifa11@gmail.com>
2022-11-29 02:22:50 +0500

    Project Cards UI updated

--- src/components/design/ProjectCard.jsx
+++ src/components/design/ProjectCard.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import { IoLogoGithub } from "react-icons/io";
+import { IoLink } from "react-icons/io5";
 
-function ProjectCard(props) {
+function ProjectCard({ data }) {
   return (
     <article
-      className='flex flex-col 
-        relative h-[350px] w-[400px] min-w-[250px]
-        p-6 rounded-[16px] bg-[#17141d] shadow-[-1rem_0_3rem_#000]
-        hover:translate-y-[-1rem] peer peer-hover:translate-x-[130px] [&:not(first-child)]:ml-[-130px]
-        transition duration-300
-        '>
-      <header className=''>
-        <p className=''>24, may 2020</p>
-        <h2 className='font-semibold text-4xl'>This is CSS cards tricks</h2>
+      className={`flex flex-col justify-end relative 
+      h-[350px] w-[350px] min-w-[250px] p-3 rounded-2xl
+      shadow-[-1rem_0_3rem_#000] bg-[url("${data.hero}")] bg-no-repeat bg-cover
+      hover:translate-y-[-1rem] peer peer-hover:translate-x-[130px]
+      [&:not(first-child)]:ml-[-130px] 
+      transition duration-300 space-y-2`}>
+      <div className='flex space-x-2'>
+        {data.repo && (
+          <a href={data.repo} className='text-4xl text-[#1c1c1c]'>
+            <IoLogoGithub />
+          </a>
+        )}
+        {data.demo && (
+          <a href={data.demo} className='text-4xl text-[#1c1c1c]'>
+            <IoLink />
+          </a>
+        )}
+      </div>
+      <header className='bg-base rounded-xl p-2 h-32 overflow-hidden'>
+        <h2 className='font-semibold text-xl text-ellipsis'>{data.title}</h2>
+        <p className='text-xs'>{data.description}</p>
+        <div className='flex mt-5 space-x-2 overflow-ellipsis'>
+          {data.tags?.map((tag, index) => {
+            return (
+              <div key={index} className='badge badge-secondary text-ellipsis'>
+                {tag}
+              </div>
+            );
+          })}
+        </div>
       </header>
     </article>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import ProjectCard from "../components/design/ProjectCard";
+import ProjectsData from "../assets/projects.json";
 
 function Projects() {
   return (
     <section
       id='projects'
-      className='w-full h-screen backdrop-blur-sm font-poppins flex flex-col justify-center'>
-      <div className='flex p-12 overflow-y-scroll scrollbar-thin scrollbar-thumb-ghost scrollbar-track-base'>
+      className='w-full h-screen backdrop-blur-sm font-poppins flex flex-col justify-between'>
+      <h1 className='text-5xl w-full text-center font-bold py-5 mt-14 bg-gray-800 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-400 '>
+        Projects
+      </h1>
+      <div className='flex p-12 overflow-y-scroll mb-20 scrollbar-thin scrollbar-thumb-ghost scrollbar-track-base'>
         <div
           className='flex flex-col 
-        relative h-[350px] w-[400px] min-w-[250px]
-        p-6 rounded-[16px] bg-[#17141d] mr-40
+        relative h-[350px] w-[300px] min-w-[250px]
+        p-6 rounded-[16px] bg-[#17141d] mr-40 
         text-4xl font-semibold'>
-          These are some of projects which are out there in the wild for the
-          peasents to see.
+          Some of the projects I've developed so far.
         </div>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {ProjectsData.map((project, key) => {
+          return <ProjectCard data={project} key={key} />;
+        })}
       </div>
     </section>
   );
