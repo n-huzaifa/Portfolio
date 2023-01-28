57d8447ba955197b47f518e9ee52806c29737585
n-huzaifa <noorhuzaifa11@gmail.com>
2023-01-29 02:18:04 +0500

    project cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import React from "react";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Skills from "./pages/Skills";
-import Contact from "./pages/Contact";
-import Particles from "./components/design/Particles";
 
 function App() {
   return (
     <>
-      <div className='w-full bg-transparent '>
-        <Particles />
-        <div className='h-full box-border'>
-          <Home />
-          <Projects />
-          <Skills />
-          <Contact />
-        </div>
-      </div>
+      <div className='w-full bg-transparent '>hello</div>
     </>
   );
 }
