b3f1ec8f664c58e8a14452f692fe94e42ddf39f9
n-huzaifa <noorhuzaifa11@gmail.com>
2023-02-01 03:52:16 +0500

    Contact page joined

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Hero from "./pages/Hero";
 import Services from "./pages/Services";
 import Portfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar";
+import Contact from "./pages/Contact";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={`${darkMode ? "dark" : ""}`}>
-      <div className='bg-white dark:bg-gray-800 px-10 md:px-20 lg:px-40'>
+      <div className='bg-[#E3FDFD] dark:bg-gray-800 dark:text-white px-10 md:px-20 lg:px-40'>
         <Navbar darkModeToggle={setDarkMode} darkModeState={darkMode} />
         <Hero />
         <Services />
         <Portfolio />
+        <Contact />
       </div>
     </div>
   );
