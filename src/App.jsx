82d2190d5bedd05dcc46dda92e46f4fd5da04e15
n-huzaifa <noorhuzaifa11@gmail.com>
2023-02-04 03:30:11 +0500

    Theme State in Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "./pages/Hero";
 import Services from "./pages/Services";
 import Portfolio from "./pages/Portfolio";
@@ -7,9 +7,18 @@ import Contact from "./pages/Contact";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    setDarkMode(JSON.parse(window.localStorage.getItem("theme")));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", darkMode);
+  }, [darkMode]);
+
   return (
     <div className={`${darkMode ? "dark" : ""}`}>
-      <div className='bg-[#E3FDFD] dark:bg-[#222831] dark:text-[#EEEEEE] px-10 md:px-20 lg:px-40'>
+      <div className='bg-[#E3FDFD] dark:bg-[#222831] dark:text-[#EEEEEE] px-10 md:px-20 lg:px-40 font-poppins'>
         <Navbar darkModeToggle={setDarkMode} darkModeState={darkMode} />
         <Hero />
         <Services />
