aea6c8d8165219a7dcb8879dc3e0b8ed994faaeb
n-huzaifa <noorhuzaifa11@gmail.com>
2023-02-04 04:54:41 +0500

    Lazy Loading Image Component

--- src/components/PortfolioList.jsx
+++ src/components/PortfolioList.jsx
@@ -5,47 +5,48 @@ import web3 from "/web3.webp";
 import web4 from "/web4.webp";
 import web5 from "/web5.webp";
 import web6 from "/web6.webp";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function PortfolioList() {
   return (
     <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
       <div className='basis-1/3 flex-1 '>
-        <img
+        <LazyLoadImage
           src={web1}
           alt=''
           className='rounded-lg object-cover w-full h-full'
         />
       </div>
       <div className='basis-1/3 flex-1 '>
-        <img
+        <LazyLoadImage
           src={web2}
           alt=''
           className='rounded-lg object-cover w-full h-full'
         />
       </div>
       <div className='basis-1/3 flex-1 '>
-        <img
+        <LazyLoadImage
           src={web3}
           alt=''
           className='rounded-lg object-cover w-full h-full'
         />
       </div>
       <div className='basis-1/3 flex-1 '>
-        <img
+        <LazyLoadImage
           src={web4}
           alt=''
           className='rounded-lg object-cover w-full h-full'
         />
       </div>
       <div className='basis-1/3 flex-1 '>
-        <img
+        <LazyLoadImage
           src={web5}
           alt=''
           className='rounded-lg object-cover w-full h-full'
         />
       </div>
       <div className='basis-1/3 flex-1 '>
-        <img
+        <LazyLoadImage
           src={web6}
           alt=''
           className='rounded-lg object-cover w-full h-full'
