7b0bb7f9e90338177248af9cc091cf23b1ebdb70
n-huzaifa <noorhuzaifa11@gmail.com>
2023-02-15 21:02:55 +0500

    skill card icons update

--- src/components/SkillCards.jsx
+++ src/components/SkillCards.jsx
@@ -6,18 +6,20 @@ function SkillCards({ skill }) {
       className={`left-32 ml-2 mr-4 
       flex flex-col justify-end relative 
       h-[300px] min-w-[250px] 
-      space-y-2 p-3 rounded-2xl dark:shadow-lg dark:shadow-gray-800
+      space-y-2 p-3 rounded-2xl shadow-lg  dark:shadow-gray-800
       bg-no-repeat bg-cover
       hover:translate-y-[-1rem] 
       peer peer-hover:translate-x-[130px]
       [&:not(first-child)]:ml-[-130px] 
-      transition duration-300`}
+      transition duration-300 bg-teal-100 dark:bg-teal-900 `}
       style={{
         backgroundImage: `url(${skill.url})`,
       }}>
       <div className='bg-base rounded-xl p-2 h-32 text-gray-100 overflow-hidden flex flex-col justify-end'>
         <div>
-          <h2 className='font-semibold text-xl text-ellipsis'>{skill.name}</h2>
+          <h2 className='font-semibold text-2xl text-gray-900 text-ellipsis'>
+            {skill.name}
+          </h2>
         </div>
       </div>
     </div>
